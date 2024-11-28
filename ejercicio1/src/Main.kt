883386656fca85232adeddd95cd42c7e6b64b2da
OSCAR\oscar <oscarpardo2006@gmail.com>
2024-11-28 12:24:36 +0100

    no message

---
+++ ejercicio1/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Ejercicio 1, calcula el área y el perímetro de un cuadrado.")
+    println("Adjunta la medida del lado del cuadrado en cm")
+    var l:Int= readln().toInt()
+    print("La superficie del cuadrado es ${l *l}")
+    print(l * l)
+    println(" El perímetro del cuadrado es ")
+    print(l * 4)
+}
